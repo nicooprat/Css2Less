c137740473819a46b424aa41c8a14b3403f381e3
Nico Prat <nicooprat@gmail.com>
2012-03-14 01:00:08 +0100

    Feature: ajout infos front

--- public/js/onload.js
+++ public/js/onload.js
@@ -1,22 +1,52 @@
 var timer;
+var output;
+var input;
 
 $(document).ready(function() {
-
+    
+    // Editor
+    
     var SCSSMode = require("ace/mode/scss").Mode;
     
     output = ace.edit("output");
     
     output.getSession().setMode(new SCSSMode());
     output.setShowPrintMargin(false);
+    output.env.editor.renderer.setHScrollBarAlwaysVisible(false);
     output.insert('#hello {\n    color: blue;\n\n    #buddy {\n    	background: red;\n    }\n}');
     
     input = ace.edit("input");
     
     input.getSession().setMode(new SCSSMode());
     input.setShowPrintMargin(false);
-    
+    input.env.editor.renderer.setHScrollBarAlwaysVisible(false);    
     input.insert('#hello {\n    color: blue;\n}\n\n#hello #buddy {\n	background: red;\n}');
     input.getSession().on('change', convert);
+    
+    // Flip
+    
+    $('#info').click(function(e)
+    {
+        if( $('body').hasClass('flip') )
+        {
+        	$('#book').css('display','block');
+            setTimeout(function()
+        	{
+        	    $('body').removeClass('flip');
+        	},10);
+        }
+        else
+        {
+        	setTimeout(function()
+        	{
+        	    $('#book').css('display','none');
+        	},500);
+            $('body').addClass('flip');
+        }
+                
+        $('html,body').animate({scrollTop:0}, 'slow');
+        e.preventDefault();
+    });
   
 });
 
